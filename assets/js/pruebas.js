74c225ca83adce8eda971b7162e7b9a46247f9ce
StivenHerrera <stiven3722@gmail.com>
2023-01-25 16:31:42 -0500

    Primera actualizacion

---
+++ assets/js/pruebas.js
@@ -0,0 +1,16 @@
+//recorderis operador terciario
+
+//Tradicional
+let a = "100";
+
+/* if (a < 10) {
+  console.log("Valor menor a 10");
+} else {
+  console.log("Valor mayor a 10");
+} */
+
+/* const resul =
+  a < 10 ? console.log("Valor menor a 10") : console.log("Valor mayor a 10");
+
+console.log(resul); */
+console.log(isNaN(a));
